a473025b28ee2d8b649477ce309e9213c830fd27
Anthony Le Goas <anthony.legoas@gmail.com>
2022-03-07 22:15:20 +0100

    ♻️ add analyse button to <Header>

--- src/app/header/Header.svelte
+++ src/app/header/Header.svelte
@@ -1,9 +1,14 @@
 <script>
-    import { Logo } from '../cdk';
+    import { Button, Logo } from '../cdk';
+
+    const websiteUsesSvelte = true;
+    $: usageText = `This website does ${websiteUsesSvelte ? '' : 'not'} use Svelte ${websiteUsesSvelte ? '😃' : '😭'}`;
 </script>
 
 <header>
     <Logo />
+    <Button>Analyze website</Button>
+    <p class="usage-text">{usageText}</p>
 </header>
 
 <style>
@@ -11,5 +16,10 @@
         display: flex;
         flex-direction: column;
         align-items: center;
+    }
+
+    p.usage-text {
+        font-size: 0.825rem;
+        color: var(--color-second);
     }
 </style>
