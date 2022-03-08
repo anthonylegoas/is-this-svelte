ba37e4eee9027da5f0bef28925dad9ef4d6237aa
Anthony Le Goas <anthony.legoas@gmail.com>
2022-03-08 18:58:44 +0100

    💄 put extension infos in a <section>

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -3,19 +3,27 @@
   import Header from './header/Header.svelte'
 </script>
 
-<Header/>
 <main>
-  <Link href="https://github.com/anthonylegoas/is-this-svelte">
-    More about the extension
-  </Link>
+  <Header/>
+  <section>
+    <Link href="https://github.com/anthonylegoas/is-this-svelte">
+      More about the extension
+    </Link>
+  </section>
 </main>
 
 <style>
   main {
-    width: 300px;
+    width: 200px;
     padding: 1rem;
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  section {
+    width: 100%;
+    border-top: 1px solid #ddd;
+    padding-top: 2rem;
   }
 </style>
