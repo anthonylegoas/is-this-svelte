26af876cf1dcc99ce0bea0f207b3c392a415605a
Anthony Le Goas <anthony.legoas@gmail.com>
2022-03-09 12:18:47 +0100

    ✨ add <ConfigForm>

---
+++ src/app/config/ConfigForm.svelte
@@ -0,0 +1,19 @@
+<script>
+    $: iconNumber = 20;
+</script>
+
+<p>Configure the animation when Svelte is detected.</p>
+<label> Icons number ({iconNumber}):
+    <input name="taille" type="range" min="1" max="50" step="1" bind:value={iconNumber} />
+</label>
+
+<style>
+    p, label {
+        color: var(--color-second);
+        margin-bottom: 0.75rem;
+    }
+
+    input[type=range] {
+        width: 175px;
+    }
+</style>
