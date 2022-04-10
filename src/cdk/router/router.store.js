3ae3fffc98795f683e23a04d03732330f3564e3d
Anthony Le Goas <anthony.legoas@gmail.com>
2022-04-10 19:14:15 +0200

    ✨ add goTo & goBack router functions

--- src/cdk/router/router.store.js
+++ src/cdk/router/router.store.js
@@ -6,4 +6,40 @@ const history = writable([]);
 // Returns the latest visited route from the history.
 const currentRoute = derived(history, ($history) => $history.slice(-1)[0]);
 
-export { currentRoute };
+/**
+ * Allow to navigate to a specific route.
+ * @param path The route path.
+ * @param comesFrom The direction from where comes form the route.
+ */
+const goTo = (path, comesFrom = "right") =>
+  history.update((value) => {
+    return [...value, { path, comesFrom }];
+  });
+
+/**
+ * Allow to navigate to the route before current one.
+ */
+const goBack = () =>
+  history.update((value) => {
+    const currentRoute = value[value.length - 1];
+    let lastRoute;
+    routePath.forEach((value, key) => {
+      if (value === currentRoute.path)
+        lastRoute =
+          key === 1 || routePath[key - 1].includes("/")
+            ? routePath[key - 1]
+            : routePath[key - 2];
+    });
+
+    return lastRoute
+      ? [
+          ...value,
+          {
+            path: lastRoute,
+            comesFrom: "left",
+          },
+        ]
+      : value;
+  });
+
+export { currentRoute, goTo, goBack };
