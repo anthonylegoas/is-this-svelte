3862220a820e15af353e6ad83997a1c8c97e6e55
Anthony Le Goas <anthony.legoas@gmail.com>
2022-04-11 10:59:10 +0200

    ✨ add <GoBack> & <GoTo> components

---
+++ src/cdk/router/GoBack.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { goBack } from '$lib/stores';
+
+	const buttonClickHandler = () => goBack();
+</script>
+
+<button
+	class="vtmn-typo_text-3 vtmn-flex vtmn-items-center"
+	on:click={buttonClickHandler}
+	data-testid="go-back__button"
+>
+	Back
+</button>
+
+<style>
+	button {
+		width: fit-content;
+	}
+</style>
---
+++ src/cdk/router/GoTo.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { goTo } from '$lib/stores';
+	import type { RouteDirection, RoutePath } from '$lib/stores';
+
+	/**
+	 * Indicates the direction of the navigation animation.
+	 */
+	export let comesFrom: RouteDirection = 'right';
+	/**
+	 * The route on which to nagivate.
+	 */
+	export let to: RoutePath;
+
+	const buttonClickHandler = () => goTo(to, comesFrom);
+</script>
+
+<button
+	class="goto-button mobile:vtmn-w-full"
+	on:click={buttonClickHandler}
+	data-testid="go-to__button"
+	{...$$restProps}
+>
+	<slot />
+</button>
+
+<style>
+	:global(.goto-button) {
+		margin: 0 auto;
+	}
+
+	@media (max-width: 599px) {
+		:global(.goto-button) {
+			width: 100%;
+		}
+	}
+</style>
