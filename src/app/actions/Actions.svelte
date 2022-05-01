138f5d4ec78a4651d1560c6d57e77275d168466f
Anthony Le Goas <anthony.legoas@gmail.com>
2022-05-01 14:06:24 +0200

    ✨ add <Actions>

---
+++ src/app/actions/Actions.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { GoTo, Link } from '../../cdk';
+</script>
+
+<GoTo to="config">Config</GoTo>
+<Link href="https://github.com/anthonylegoas/is-this-svelte">
+    More about the extension
+</Link>
