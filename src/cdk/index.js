78ba05afc4d3d97c3ae2b1ddfb3ce342e0adf30a
Anthony Le Goas <anthony.legoas@gmail.com>
2022-05-06 16:38:04 +0200

    ✨ add <Icon>

--- src/cdk/index.js
+++ src/cdk/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from "./button/Button.svelte";
+export { default as Icon } from "./icon/Icon.svelte";
 export { default as Link } from "./link/Link.svelte";
 export { default as Logo } from "./logo/Logo.svelte";
