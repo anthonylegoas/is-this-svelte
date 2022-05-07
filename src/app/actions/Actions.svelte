d60d7e2867139e7348ee67d6b2800b527c228835
Anthony Le Goas <anthony.legoas@gmail.com>
2022-05-07 22:02:07 +0200

    ✨ add github icon to "about" button

--- src/app/actions/Actions.svelte
+++ src/app/actions/Actions.svelte
@@ -1,8 +1,9 @@
 <script>
-    import { GoTo, Link } from '../../cdk';
+    import { GoTo, Icon, Link } from '../../cdk';
 </script>
 
 <GoTo to="config">Config</GoTo>
 <Link href="https://github.com/anthonylegoas/is-this-svelte">
+    <Icon name="chevron-right" size="55"/>
     More about the extension
 </Link>
