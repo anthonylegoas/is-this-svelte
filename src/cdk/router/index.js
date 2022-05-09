4690d241c84b4583c5a069df3215466142c9f85b
Anthony Le Goas <anthony.legoas@gmail.com>
2022-05-09 18:16:05 +0200

    ♻️ export router components from cdk

--- src/cdk/router/index.js
+++ src/cdk/router/index.js
@@ -1,2 +1,4 @@
+export { default as GoBack } from "./GoBack.svelte";
+export { default as GoTo } from "./GoTo.svelte";
 export { default as Route } from "./Route.svelte";
 export { default as Router } from "./Router.svelte";
