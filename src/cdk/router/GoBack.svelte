e25a902d74b02962a41fbc5e52a161e1163dd15a
Anthony Le Goas <anthony.legoas@gmail.com>
2022-05-23 23:19:27 +0200

    💄 update <GoBack> style

--- src/cdk/router/GoBack.svelte
+++ src/cdk/router/GoBack.svelte
@@ -5,7 +5,6 @@
 </script>
 
 <button
-	class="vtmn-typo_text-3 vtmn-flex vtmn-items-center"
 	on:click={buttonClickHandler}
 	data-testid="go-back__button"
 >
@@ -13,7 +12,19 @@
 </button>
 
 <style>
-	button {
-		width: fit-content;
-	}
+    button {
+        background-color: #fff;
+		border: none;
+        color: var(--color-primary);
+        font-size: 1rem;
+        max-width: 15em;
+        padding: 0.5rem 1rem;
+        border-radius: 4px;
+        transition: all 0.2s ease-in-out;
+        cursor: pointer;
+    }
+
+    button:hover {
+        text-decoration: underline;
+    }
 </style>
