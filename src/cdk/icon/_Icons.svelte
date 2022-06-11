ee435b5671393d7cd8fe7ab336a3104175cf95ff
Anthony Le Goas <anthony.legoas@gmail.com>
2022-06-11 22:51:24 +0200

    🍱 update chevron-right icon size

--- src/cdk/icon/_Icons.svelte
+++ src/cdk/icon/_Icons.svelte
@@ -2,7 +2,7 @@
     <!-- wrapper div allows use of innerHTML -->
     <!-- icons from https://thenounproject.com/khuzema/collection/blobby-icon-set/ -->
     <svg>
-        <symbol viewBox="0 0 1500 1500" fill="none" id="chevron-right" class="icon">
+        <symbol viewBox="10 25 800 5" fill="none" id="chevron-right" class="icon">
             <path stroke-width="100" d="M388.819,239.537L156.092,6.816c-9.087-9.089-23.824-9.089-32.912,0.002
             c-9.087,9.089-9.087,23.824,0.002,32.912l216.27,216.266L123.179,472.272c-9.087,9.089-9.087,23.824,0.002,32.912
             c4.543,4.544,10.499,6.816,16.455,6.816c5.956,0,11.913-2.271,16.457-6.817L388.819,272.45c4.366-4.364,6.817-10.283,6.817-16.455
