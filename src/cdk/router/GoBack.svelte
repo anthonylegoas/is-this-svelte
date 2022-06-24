9e31bc308ef2aa054d0d679ecaa2c5b4137cb43a
Anthony Le Goas <anthony.legoas@gmail.com>
2022-06-24 17:00:54 +0200

    💄 update <GoBack> style

--- src/cdk/router/GoBack.svelte
+++ src/cdk/router/GoBack.svelte
@@ -1,5 +1,6 @@
 <script>
-	import { goBack } from './router.store'
+	import { goBack } from './router.store';
+    import Icon from '../icon/Icon.svelte'
 
 	const buttonClickHandler = () => goBack();
 </script>
@@ -8,23 +9,27 @@
 	on:click={buttonClickHandler}
 	data-testid="go-back__button"
 >
-	Back
+<Icon name="chevron-left" size="12"/>
+<span>Back</span>
 </button>
 
 <style>
     button {
+		    width: 100%;
         background-color: #fff;
-		border: none;
+		    border: none;
         color: var(--color-primary);
-        font-size: 1rem;
-        max-width: 15em;
-        padding: 0.5rem 1rem;
+        font-size: 0.75rem;
+        padding: 0.5rem 0;
         border-radius: 4px;
         transition: all 0.2s ease-in-out;
         cursor: pointer;
+		    display: flex;
+		    align-items: center;
+		    justify-content: flex-start;
     }
 
     button:hover {
-        text-decoration: underline;
+		    background-color: rgba(255, 62, 0, .1);
     }
 </style>
