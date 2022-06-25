ee31744bffa7740d43b7b1794a5214485eba15dd
Anthony Le Goas <anthony.legoas@gmail.com>
2022-06-25 22:33:53 +0200

    ♻️ update <About>

--- src/app/about/About.svelte
+++ src/app/about/About.svelte
@@ -2,6 +2,14 @@
     import { Link } from '../../cdk';
 </script>
 
+<p>Made with ❤️ by <a href="https://github.com/anthonylegoas">@anthonylegoas</a></p>
+
 <Link href="https://github.com/anthonylegoas/is-this-svelte">
-    More about the extension
+    See on GitHub
 </Link>
+
+<style>
+    p {
+        color: var(--color-primary);
+    }
+</style>
